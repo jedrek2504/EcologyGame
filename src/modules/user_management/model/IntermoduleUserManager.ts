b3e0ec62261bf3e8e56651edd3470d8e5a6c099d
mundus <mundus@pc6.home>
2023-12-14 18:41:42 +0100

    Added comment for IntermoduleUserManager::getEventStream

--- src/modules/user_management/model/IntermoduleUserManager.ts
+++ src/modules/user_management/model/IntermoduleUserManager.ts
@@ -3,5 +3,12 @@ import {EventStream} from "../lib/EventStream.js"
 import { UMMEventFilter } from "./UMMEventFilter.js"
 export interface IntermoduleUserManager {
     getUsers(filtrator: (user : User) => boolean) : User[];
+
+    /**
+     * 
+     * @param streamFilter 
+     * @note THE STREAM RETURNED BY THIS FUNCTION IS ENTIRELY FOR BACKEND-BACKEND COMMUNICATION. 
+     *       If you want to pass the stream to your module's frontend, you need to implement SSE (Server Sent Events) or Websocket on the backend and frontend side of your module
+     */
     getEventStream(streamFilter : UMMEventFilter) : EventStream;
 }
