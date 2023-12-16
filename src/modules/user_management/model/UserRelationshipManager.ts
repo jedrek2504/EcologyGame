9a2603cbea512ab6ac3442132c7fe0bb885a51a9
mundus <mundus@pc6.home>
2023-12-16 19:14:39 +0100

    During UserRelationshipManager::listUserRelationships implementation. Branch paused due to hot integration fix required

--- src/modules/user_management/model/UserRelationshipManager.ts
+++ src/modules/user_management/model/UserRelationshipManager.ts
@@ -1,14 +1,53 @@
 import { IntermoduleUserRelationshipManager } from "./IntermoduleUserRelationshipManager.js";
 import {User} from "./User.js";
 import { Relationship } from "./Relationship.js";
+import db from "../database.js";
+import { Op } from "sequelize";
 export class UserRelationshipManager implements IntermoduleUserRelationshipManager {
-    createRelationship(source : User, target : User) : Relationship | null {
-        return null;
+    /**
+     * 
+     * @param source 
+     * @param target 
+     * @returns Relationship instance or null if the database query failed
+     */
+    createRelationship(source : User, target : User) : Relationship | null{
+        if (source.getId() == target.getId()) {
+            throw Error("Cannot create user relation with himself");
+        }
+        let relationship = new Relationship(source, target);
+        if (relationship.executeBind()) {
+            return relationship;
+        }
+        else {
+            return null;
+        }
     }
+
+    /**
+     * 
+     * @param relationship 
+     * @returns True if the detabase query was successfully executed
+     */
     deleteRelationship(relationship : Relationship) : boolean {
-        return false;
+        return relationship.destroy();
     }
-    listUserRelationships(target : User) : Relationship[] {
+
+    /**
+     * 
+     * @param target 
+     * @returns Array of relationship instances connected to the given user
+     * or null if database failed 
+     */
+    listUserRelationships(target : User) : Relationship[] | null {
+        let targetID : Number = target.getId() as unknown as Number;
+        db.Relationship.findAll({
+            where: {
+                [Op.or]: [
+                  { a: targetID },
+                  { b: targetID }
+                ]
+            }
+        });
         return [];
     }
 
