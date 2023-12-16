c405049826d07d3836ebe6a0ffe0ae8a0d05396f
adamski234 <adamski234@protonmail.com>
2023-12-16 02:15:04 +0100

    Implemented UserManager

--- src/modules/user_management/model/UserManager.ts
+++ src/modules/user_management/model/UserManager.ts
@@ -5,53 +5,70 @@ import { IntermoduleUserManager } from "./IntermoduleUserManager.js";
 import { User } from "./User.js";
 import { UMMEventFilter } from "./UMMEventFilter.js";
 import {EventStream} from "../lib/EventStream.js";
+import utils from "../utils.js";
 
 export class UserManager implements IntermoduleUserManager {
     getUsers(filtrator : (user : User) => boolean) : User[] {
         return [];
     }
-    createUser(username : string, 
-               email : string, 
-               password : string) : Promise<User | null> {
-        
-        return new Promise(async (resolve, reject)=> {
-            if (await db.Person.findOne({where: {username: username}})) {
-                reject("Trying to create existing user");
-            }
-            else if (await db.Person.findOne({where: {email: email}})) {
-                reject("Email is already taken");
-            }
-            else {
-                db.Person.create({
-                    username: username,
-                    password: password,
-                    email: email
-                });
-                resolve(null); // Dummy / Why would I return a User instance?
-            }
-        });
+    async createUser(username: string, email: string, password: string): Promise<User | null> {
+        if (await db.Person.findOne({where: {username: username}})) {
+			return null; //Trying to create existing user
+		} else if (await db.Person.findOne({where: {email: email}})) {
+			return null; //Email is already taken
+		} else {
+			await db.Person.create({
+				username: username,
+				password: password,
+				email: email,
+			});
+			// automatically log in
+			return await this.login(username, password);
+		}
     }
 
-    deleteUser(user : User) : boolean {
-        return false;
+    async deleteUser(user: User): Promise<boolean> {
+        const userInstance = await db.Person.findByPk(user.getId());
+		const wasFound = userInstance != null;
+		await userInstance?.destroy();
+		return wasFound;
     }
 
-    login(username : string, password : string) : User | null {
-        return null;
+    async login(username: string, password: string): Promise<User | null> {
+		const userInstance: any = await db.Person.findOne({
+			where: {
+				"username": username,
+				"password": password,
+			}
+		});
+		if (userInstance == null) return null;
+		let generatedId;
+		do {
+			generatedId = utils.makeid(64);
+		} while (await db.LoginInstance.findByPk(generatedId));
+		const login: any = await db.LoginInstance.create({
+			login_id: generatedId,
+			user_id: userInstance.user_id
+		});
+        return new User(userInstance.user_id);
     }
 
-    logout(target : User) : void {
-        
+    async logout(target: User): Promise<void> {
+		await db.LoginInstance.destroy({
+			where: {
+				"user_id": target.getId(),
+			}
+		})
     }
 
-    getEventStream(streamFilter : UMMEventFilter) : EventStream {
+    getEventStream(streamFilter: UMMEventFilter): EventStream {
         return EventStream.getInstance();
     }
     private constructor() {
         
     }
     private static _instance: UserManager
-    public static getInstance () : UserManager {
+    public static getInstance(): UserManager {
       if (!this._instance) this._instance = new UserManager()
       return this._instance
     }
