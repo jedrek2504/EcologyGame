b1be62c661ad2aa8b03e108219626f3667dac230
Adam Banaś <242353@edu.p.lodz.pl>
2023-12-17 23:21:23 +0100

    Created frontend for LBM displaying list of users acquired from UMM interface

--- src/modules/leaderboard/routes/board.ts
+++ src/modules/leaderboard/routes/board.ts
@@ -1,13 +1,16 @@
 
 import express from 'express';
 const router = express.Router();
+import {topUsersExport as top} from "../test.js";
 
 router.get('/leaderboard', (req, res) => {
     res.sendFile('leaderboard.html', { root: './public' });
 });
 
+router.get('/json/leaderboard', (req, res) => {
+   res.json(top);
+});
+
 export default {
-	router: router,
+    router: router,
 }
-
-
