6a0d633646ac68dddaa1770dd2e3dc3365e04ee2
Jedrzej Kostyk <jedrek2504@gmail.com>
2023-12-17 20:58:09 +0100

    Handling routing for ForumModule

--- src/modules/forum/routes/forum.ts
+++ src/modules/forum/routes/forum.ts
@@ -1,14 +1,32 @@
-/* forum/routes/forum.ts */
 import express from 'express';
 import UMM from "../../user_management/exports/api.js"
-import {ForumUser} from "../../user_management/model/ForumUser.js";
+import { ForumUser } from "../../user_management/model/ForumUser.js";
 
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    let user: ForumUser = <ForumUser>await UMM.IntermoduleCommons.IntermoduleUserManager.getUserBySessionKey(req.cookies["login_id"]) // important
+    try {
+        // Authenticate and get the user
+        let user: ForumUser = <ForumUser>await UMM.IntermoduleCommons.IntermoduleUserManager.getUserBySessionKey(req.cookies["login_id"]);
 
-    res.render('forum.html', {title: 'Express'});
+        if (!user) {
+            // Handle unauthenticated user
+            return res.redirect('/login');
+        }
+
+        // Get forum posts for the user
+        const posts = await UMM.ForumMediator.postList(user);
+
+        // Render the forum page with posts
+        res.render('forum.html', {
+            title: 'Forum',
+            posts: posts,
+        });
+    } catch (error) {
+        // Handle errors, such as database connection issues
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 export default {
