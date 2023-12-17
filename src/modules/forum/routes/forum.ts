bd5c0fabf6b77d4c5dce1c131c56cffd852f6721
Jedrzej Kostyk <jedrek2504@gmail.com>
2023-12-17 22:05:47 +0100

    Added new post creation form, Route to handle new post creation WIP

--- src/modules/forum/routes/forum.ts
+++ src/modules/forum/routes/forum.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import UMM from "../../user_management/exports/api.js"
 import { ForumUser } from "../../user_management/model/ForumUser.js";
+import {ForumModule} from "../model/ForumModule.js";
+import {ForumPost} from "../model/ForumPost.js";
+import {PostList} from "../model/PostList.js";
 
 const router = express.Router();
-
+const postList = new PostList()
+const forumModule = new ForumModule(postList)
 router.get('/', async function (req, res) {
     try {
         // Authenticate and get the user
@@ -14,7 +18,7 @@ router.get('/', async function (req, res) {
             return res.redirect('/login');
         }
 
-        // Get forum posts for the user
+        // todo - to powinno byc nadpisane przez odpowiednia metode postList z naszego modulu
         const posts = await UMM.ForumMediator.postList(user);
 
         // Render the forum page with posts
@@ -28,6 +32,30 @@ router.get('/', async function (req, res) {
         res.status(500).send('Internal Server Error');
     }
 });
+
+// Route to handle new post creation
+router.post('/post', async (req, res) => {
+    try {
+        const user: ForumUser = <ForumUser>await UMM.IntermoduleCommons.IntermoduleUserManager.getUserBySessionKey(req.cookies["login_id"]);
+        if (!user) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const { title, content } = req.body;
+        const forumPost = new ForumPost(title, content, user);
+        const postCreated = await forumModule.addPost(user, forumPost);
+
+        if (!postCreated) {
+            res.redirect('/forum');
+        } else {
+            res.status(500).send('Failed to create post');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 export default {
     router: router
