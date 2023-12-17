d2ce056794d5fdfa508bda5116c80f09972f1998
mundus <mundus@pc6.home>
2023-12-17 22:25:34 +0100

    Fix GM faults from merge

--- src/config.sh
+++ src/config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-export SQLITE_DBS_LOCATION="/Users/krzysztof/ecopers/sqlite/databases"
+export PERSISTENT_VOL_LOCATION="/ecopers"
+export SQLITE_DBS_LOCATION="/ecopers/sqlite/databases"
 #export USRFILES_LOCATION="/pers/usrfiles"
