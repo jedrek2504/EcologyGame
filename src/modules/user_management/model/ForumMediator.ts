50c0ace1fbb24d256f179ef7f69cbdaee388a05c
mundus <mundus@pc6.home>
2023-12-18 00:14:23 +0100

    ForumMediator::registerPost integration fix for Forum module

--- src/modules/user_management/model/ForumMediator.ts
+++ src/modules/user_management/model/ForumMediator.ts
@@ -1,7 +1,7 @@
 import {ForumUser} from './ForumUser.js'
 import { Postable } from './Postable.js';
 export interface ForumMediator {
-    registerPost(user : ForumUser, post : /*Object*/Postable) : Promise<boolean>;
+    registerPost(user : ForumUser, post : /*Object*/Postable) : Promise<string>;
     unregisterPost(user : ForumUser, post : /*Object*/Postable) : Promise<boolean>;
     postList(user : ForumUser) : Promise</*Object[]*/Postable[]>;
 }
