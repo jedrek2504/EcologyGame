212715139ab74c686bbd56ef9da95b24b914bf7b
Adam Banaś <242353@edu.p.lodz.pl>
2023-12-17 18:55:09 +0100

    UMM connection test

--- src/modules/leaderboard/routes/board.js
+++ src/modules/leaderboard/routes/board.js
@@ -1,13 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
+import express from 'express';
+const router = express.Router();
 router.get('/leaderboard', (req, res) => {
     res.sendFile('leaderboard.html', { root: './public' });
 });
-exports.default = {
+export default {
     router: router,
 };
