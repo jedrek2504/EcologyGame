1cc7be53915b7c89d6892d090ef427304f684208
mundus <mundus@pc6.home>
2024-01-21 19:19:06 +0100

    Added community explorer

--- src/modules/user_management/routes/explorer.ts
+++ src/modules/user_management/routes/explorer.ts
@@ -9,8 +9,33 @@ import UMM from '../exports/api.js';
 const router = express.Router();
 
 router.get('/community', async (req: any, res: any, next: any) => {
-    res.render('community', { title: 'Community' });
+    // Put all {user_id, photo} into an array at res.locals.users
+    UMM.IntermoduleCommons.IntermoduleUserManager.getUsers((u:User)=>true).then(async(users : User[]) => {
+        res.locals.users = await Promise.all(users.map(async (u) => {
+            return {
+                user_id: u.getId(),
+                photo: await u.getPhoto()
+            };
+        }));
+
+
+        /*res.locals.users = users.map(async (u) => {
+            return {
+                user_id: u.getId(),
+                photo: await u.getPhoto()
+            };
+        });*/
+        console.log("Users:");
+        console.log(res.locals.users);
+        res.render('community', { title: 'Community' });
+    }).catch((error : any) => {
+        console.log(`Error while fetching users: ${error}`);
+        res.locals.error = `Error while fetching users: ${error}`;
+        res.render('community', { title: 'Community' });
+    });
 });
+
+
 
 export default {
     router: router
